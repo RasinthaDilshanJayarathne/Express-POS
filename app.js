396969530ccfd25006e766bf681a7ebb969df657
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-02 10:20:17 +0530

    send customer post request

--- app.js
+++ app.js
@@ -7,9 +7,17 @@ app.get('/',(req,res) =>{
     res.send('get req came for / route')
 });
 
+app.post('/',(req,res) =>{
+    res.send('<h2>post request came for /route<h2/>')
+});
+
 app.get('/customer',(req,res) =>{
     console.log('get request comming!');
     res.send('<h1>Customer get request came !<h1/>')
+});
+
+app.post('/customer',(req,res) =>{
+    res.send('<h2>post request came for /customer <h2/>')
 });
 
 app.get('/item',(req,res) =>{

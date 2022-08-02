5ae2fe180f2302534a488f9c360bedb639237353
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-02 09:38:57 +0530

    Customer Request

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+app.get('/',(req,res) =>{
+    console.log('get request comming!');
+    res.send('get req came for / route')
+});
+
+app.get('/customer',(req,res) =>{
+    console.log('get request comming!');
+    res.send('<h1>customer get request came <h1/>')
+});
+
 app.listen(port, () =>{
-    console.log(`app listening on port ${port}`)
-})
+    console.log(`app listening on port ${port}`);
+});

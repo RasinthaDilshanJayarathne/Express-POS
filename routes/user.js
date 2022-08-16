4139cd4137e2b231b89d10a3aa89a59fe0e8c122
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-16 09:08:28 +0530

    User CRUD

--- routes/user.js
+++ routes/user.js
@@ -9,4 +9,16 @@ router.post('/',(req, res) =>{
     res.send('user post method')
 })
 
+router.put('/',(req, res) =>{
+    res.send('user post method')
+})
+
+router.delete('/:id', (req, res) => {
+    res.send('delete user-id request came')
+})
+
+router.get('/:id', (req, res) => {
+    res.send('get user-id request came')
+})
+
 module.exports = router;

80ae6a3192d59105dcb8e63152f6b75f5fbf17bf
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-16 09:28:08 +0530

    Updating

--- routes/user.js
+++ routes/user.js
@@ -6,17 +6,20 @@ router.get('/',(req, res) =>{
 })
 
 router.post('/',(req, res) =>{
+    console.log(req.body);
     res.send('user post method')
 })
 
 router.put('/',(req, res) =>{
-    res.send('user post method')
+    res.send('user put method')
 })
 
 router.delete('/:id', (req, res) => {
+    console.log(req.params.id);
     res.send('delete user-id request came')
 })
 
+//get user by id
 router.get('/:id', (req, res) => {
     res.send('get user-id request came')
 })

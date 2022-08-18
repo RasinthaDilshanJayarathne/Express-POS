ffdc45e9e7a43d92673dd76fa2088dc5ea11bc8d
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-18 13:36:06 +0530

    Updating

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const item = require('./routes/item')
 const user = require ('./routes/user')
 const order = require ('./routes/Order')
 
+
 const app = express()
 const port = 4000
 
@@ -14,6 +15,7 @@ app.use('/customer', customer)
 app.use('/items',  item)
 app.use('/users',user)
 app.use('/orders',order)
+
 
 //const url = 'mongodb://127.0.0.1/express'
 

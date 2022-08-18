975e404d3b005f073365ee15347f7de9ca33e3b8
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-08-18 08:33:40 +0530

    Finished Order CRUD

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const customer = require('./routes/customer')
 const item = require('./routes/item')
 const user = require ('./routes/user')
+const user = require ('./routes/Order')
 const app = express()
 const port = 4000
 
@@ -9,6 +10,7 @@ app.use(express.json())
 app.use('/customer', customer)
 app.use('/items',  item)
 app.use('/users',user)
+app.use('/orders',order)
 
 // app.get('/', (req, res) => {
 //     console.log('get request comming!');
